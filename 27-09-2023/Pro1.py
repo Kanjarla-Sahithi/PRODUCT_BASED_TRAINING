d15f43b35556848dee7ae371c453ec0fdff73300
Kanjarla Sahithi <108618620+Kanjarla-Sahithi@users.noreply.github.com>
2023-09-27 20:31:16 +0530

    Pro1.py

---
+++ 27-09-2023/Pro1.py
@@ -0,0 +1,8 @@
+class Program1:
+    def pro1(pro,a):   #pro is like a self keyword parameter 
+        print("Hello",a)
+    def pro2(pro,b):
+        print("27-09-2023",b)
+obj=Program1()  
+obj.pro1('- first day')
+obj.pro2(1)
