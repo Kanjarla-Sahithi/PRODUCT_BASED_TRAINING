557453aab14601e6bb360a34130b5dffc92d8113
Kanjarla Sahithi <108618620+Kanjarla-Sahithi@users.noreply.github.com>
2023-10-03 11:05:45 +0530

    MCQ's_Ex.c

---
+++ 29-09-2023/MCQ's_Ex.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+/*
+main()
+{
+  if(-100)     //it takes the only non zero values means expect zero but negative and positive number
+  {
+   printf("TEST1");
+  }
+  else
+  {
+   printf("TEST2");
+  }
+}*/
+
+/*
+main()
+{
+  if(printf(0))
+  {
+   printf("TEST1");
+  }
+  else
+  {
+   printf("TEST2");
+  }
+}*/
+/*main()
+{
+  //if(printf("hello",printf("bye",printf("check",printf("%d",0)))))
+  if(printf(0))
+  {
+   printf("TEST1");
+  }
+  else
+  {
+   printf("TEST2");
+  }
+}*/
+
+
+/*
+main()
+{
+  if(printf("")) // because in printf there is no value so it return to else condition
+  {
+   printf("TEST1");
+  }
+  else
+  {
+   printf("TEST2");
+  }
+}
+*/
+
+/*
+void main()
+{
+  if(printf("%d",1))
+  {
+   printf("TEST1 GOOD");
+  }
+  else
+  {
+   printf("TEST2");
+  }
+}*/
