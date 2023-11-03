b38417a6c2869ef03ac2a278abaf54e594a514b2
Kanjarla Sahithi <108618620+Kanjarla-Sahithi@users.noreply.github.com>
2023-11-03 21:52:53 +0530

    Add files via upload

---
+++ 14-10-2023/Prime_Algorithm.py
@@ -0,0 +1,16 @@
+#Prime number or not
+def Sieve0fEratosthenes(num):
+    prime=[True]*(num+1)
+    p=2
+    while(p*p<=num):
+        if prime[p]:
+            for i in range(p*p,num+1,p):
+                prime[i]=False
+        p+=1
+    for p in range(2, num+1):
+        if prime[p]:
+            print(p)
+            
+num=30
+Sieve0fEratosthenes(num)
+        
